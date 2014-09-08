1370d962f3d4f7b580aa8a96b77ba6a90f82ae56
Andreas Pohl <pohl@adtech.de>
2014-09-08 10:22:56 +0200

    setup: override existing config files

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ fi
 if [ ! -e $HOME/bin/tools ]; then
     ln -s $(pwd) $HOME/bin/tools
 fi
-ln -s $(pwd)/.emacs $HOME/
-ln -s $(pwd)/.bashrc $HOME/
+ln -sf $(pwd)/.emacs $HOME/
+ln -sf $(pwd)/.bashrc $HOME/
 ln -sf $HOME/.bashrc $HOME/.profile
