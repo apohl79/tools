253b5b6561b4e1166f1f7a98e079c80a5d8379e3
Andreas Pohl <pohl@adtech.de>
2015-12-15 20:43:25 +0100

    added cpp file templates

---
+++ emacs.d/templates/1000:.*.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) `(format-time-string "%Y")` Andreas Pohl
+ * Licensed under MIT (see COPYING)
+ *
+ * Author: Andreas Pohl
+ */
+
+#ifndef ${1:`(upcase (file-name-nondirectory (file-name-sans-extension (buffer-file-name))))`_H}
+#define $1
+
+/*
+ * ${2:`(file-name-nondirectory (file-name-sans-extension (buffer-file-name)))`} class
+ *
+ * \brief Description for $2
+ */
+class $2 {
+  public:
+    $2();
+    virtual ~$2();
+$0
+};
+
+#endif  // $1
