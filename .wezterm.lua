7cb81f521ab47c2f0cfc1f6b1791e83f68e73385
Andreas Pohl <andreas.pohl@syncdna.com>
2024-07-07 14:40:32 +0200

    updates

---
+++ .wezterm.lua
@@ -0,0 +1,151 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Andromeda'
+config.window_decorations = "RESIZE"
+config.use_fancy_tab_bar = false
+config.tab_max_width = 32
+config.colors = {
+  tab_bar = {
+    background = '#1a1e27',
+    active_tab = {
+      bg_color = '#262a33',
+      fg_color = '#c0c0c0',
+    },
+    inactive_tab = {
+      bg_color = '#1a1e27',
+      fg_color = '#808080',
+    },
+    new_tab = {
+      bg_color = '#1a1e27',
+      fg_color = '#808080',
+    }
+  }
+}
+
+local act = wezterm.action
+config.keys = {
+  -- remove keys I need in emacs
+  {
+    key = '_',
+    mods = 'CTRL|SHIFT',
+    action = act.DisableDefaultAssignment,
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'CTRL|SHIFT',
+    action = act.DisableDefaultAssignment,
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CTRL|SHIFT',
+    action = act.DisableDefaultAssignment,
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CTRL|SHIFT',
+    action = act.DisableDefaultAssignment,
+  },
+  {
+    key = 'UpArrow',
+    mods = 'CTRL|SHIFT',
+    action = act.DisableDefaultAssignment,
+  },
+  -- split
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'd',
+    mods = 'CMD|SHIFT',
+    action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  -- pane selection
+  {
+    key = '9',
+    mods = 'CTRL',
+    action = act.PaneSelect {
+      alphabet = '123456789',
+    },
+  },
+  -- show the pane selection mode, but have it swap the active and selected panes
+  {
+    key = '0',
+    mods = 'CTRL',
+    action = act.PaneSelect {
+      alphabet = '123456789',
+      mode = 'SwapWithActive',
+    },
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'CMD|META',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CMD|META',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'UpArrow',
+    mods = 'CMD|META',
+    action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CMD|META',
+    action = act.ActivatePaneDirection 'Down',
+  },
+  -- resize panes
+  {
+    key = 'UpArrow',
+    mods = 'CMD|SHIFT',
+    action = act.AdjustPaneSize { 'Up', 1 },
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CMD|SHIFT',
+    action = act.AdjustPaneSize { 'Down', 1 },
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'CMD|SHIFT',
+    action = act.AdjustPaneSize { 'Left', 1 },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CMD|SHIFT',
+    action = act.AdjustPaneSize { 'Right', 1 },
+  },
+  -- tab selection
+  {
+    key = 'LeftArrow',
+    mods = 'CMD',
+    action = act.ActivateTabRelative(-1),
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CMD',
+    action = act.ActivateTabRelative(1),
+  },
+}
+
+config.mouse_bindings = {
+  -- Scrolling up/down while holding CMD adjusts the panel size
+  {
+    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    mods = 'CMD',
+    action = act.AdjustPaneSize { 'Left', 1 },
+  },
+  {
+    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    mods = 'CMD',
+    action = act.AdjustPaneSize { 'Right', 1 },
+  },
+}
+
+
+return config
