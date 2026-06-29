fe263725128b3bcdaacf275f71972a57cd57612d
Andreas Pohl <247204286+andreas-pohl-parloa@users.noreply.github.com>
2026-06-29 19:29:53 +0200

    chore: remove vendored markdown preview server

--- markdown-preview-server/src/html.ts
+++
@@ -1,8 +0,0 @@
-// src/html.ts
-export function escapeHtml(s: string): string {
-  return s
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;');
-}
